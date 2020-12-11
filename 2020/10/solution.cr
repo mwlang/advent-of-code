9e249ec4bd1a8eada93291696f075841ae19c0eb
Michael Lang <mlang@newrelic.com>
2020-12-10 23:13:13 -0500

    Ruby and Crystal implementations benchmarked

--- 2020/10/solution.cr
+++ 2020/10/solution.cr
@@ -28,17 +28,27 @@ class Adapter
   end 
 end
 
-data = File.read_lines("input.txt").map(&.to_i).sort
-data.unshift(0)
-data << (data[-1] + 3)
+require "benchmark"
 
-adapters = data.sort.map{|jolt| Adapter.new jolt}
-adapters.each_with_index do |adapter, index|
-  adapters[[index - 3, 0].max, 3].each{|a| a << adapter}
+Benchmark.bm do |x|
+  x.report("Crystal") do
+    data = File.read_lines("input.txt").map(&.to_i).sort
+    data.unshift(0)
+    data << (data[-1] + 3)
+
+    adapters = data.sort.map{|jolt| Adapter.new jolt}
+    adapters.each_with_index do |adapter, index|
+      adapters[[index - 3, 0].max, 3].each{|a| a << adapter}
+    end
+
+    adapters.each do |adapter|
+      puts adapter.to_s
+    end
+
+    diffs = adapters.each_cons(2).map{|(a,b)| b.jolt - a.jolt}.partition{|p| p == 1}.map(&.size)
+    puts diffs.join(",")
+    puts diffs[0] * diffs[1]
+
+    puts adapters[0].compute adapters[-1]
+  end
 end
-
-diffs = adapters.each_cons(2).map{|(a,b)| b.jolt - a.jolt}.partition{|p| p == 1}.map(&.size)
-puts diffs.join(",")
-puts diffs[0] * diffs[1]
-
-puts adapters[0].compute adapters[-1]
