d632ae03f66711efc7e463c143cdf21bb4a03fd4
mwlang <mwlang@cybrains.net>
2020-12-15 07:17:22 -0500

    Day 15

---
+++ 2020/15/solution.rb
@@ -0,0 +1,38 @@
+# In this game, the players take turns saying numbers. They begin by taking turns
+# reading from a list of starting numbers (your puzzle input). Then, each turn
+# consists of considering the most recently spoken number:
+
+# If that was the first time the number has been spoken, the current player says 0.
+
+# Otherwise, the number had been spoken before; the current player announces how
+# many turns apart the number is from when it was previously spoken.
+
+# So, after the starting numbers, each turn results in that player speaking aloud
+# either 0 (if the last number is new) or an age (if the last number is a
+# repeat).
+
+def play starting, turns
+  collection = {}
+  numbers = starting.map.with_index{ |v,t| collection[v] = [t + 1]}
+  numbers = starting.reverse
+
+  (turns - numbers.size).times do |turn|
+    current_turn = starting.size + turn + 1
+    current = collection[numbers[0]]
+
+    if current.size == 1
+      numbers.unshift(0)
+      collection[0] << current_turn
+    else
+      age = current[-1] - current[-2]
+      numbers.unshift(age)
+      collection[age] ||= []
+      collection[age] << current_turn
+    end
+  end
+  numbers[0]
+end
+
+puts play [0,3,6], 10
+puts play [2,20,0,4,1,17], 2020
+puts play [2,20,0,4,1,17], 30000000
