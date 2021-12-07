96cf0bfd5d4285250271acbf6809a9bbcb2d3694
mwlang <mwlang@cybrains.net>
2021-12-07 07:44:57 -0500

    better math for part 2

--- 2021/07/solution.rb
+++ 2021/07/solution.rb
@@ -6,15 +6,23 @@ crab_positions.each do |position|
   crabbies[position] += 1
 end
 
+# part 1 computation
 def alpha(plane, current)
   (plane - current).abs
 end
 
+# original part 2 computation using recursion
 def delta plane, current, step = 1
   return 0 if plane == current
 
   direction = current > plane ? -1 : 1
   step + delta(plane, current + direction, step + 1)
+end
+
+# using math therom for part 2 is _much_ faster
+def gamma plane, current
+  n = alpha(plane, current)
+  n * (n + 1) / 2
 end
 
 def compute crabbies
@@ -25,4 +33,4 @@ def compute crabbies
 end
 
 pp compute(crabbies){ |plane, current| alpha(plane, current) }[0]
-pp compute(crabbies){ |plane, current| delta(plane, current) }[0]
+pp compute(crabbies){ |plane, current| gamma(plane, current) }[0]
